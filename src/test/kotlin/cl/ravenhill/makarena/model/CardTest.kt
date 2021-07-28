345a18e82cdb0b6675aa72e4c19229d607c37f15
R8V <islaterm@gmail.com>
2021-07-28 17:56:41 -0400

    shore(cards): Sync

--- src/test/kotlin/cl/ravenhill/makarena/model/CardTest.kt
+++ src/test/kotlin/cl/ravenhill/makarena/model/CardTest.kt
@@ -4,9 +4,7 @@ package cl.ravenhill.makarena.model
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.property.Arb
-import io.kotest.property.arbitrary.codepoints
 import io.kotest.property.arbitrary.int
-import io.kotest.property.arbitrary.string
 import io.kotest.property.forAll
 
 
@@ -35,8 +33,5 @@ class CardTest : StringSpec({
     }
   }
 })
-
-fun nameGenerator() =
-  Arb.string(minSize = 1, maxSize = 100, codepoints = Arb.Companion.codepoints())
 
 fun attackGenerator() = Arb.int(0, 10)
