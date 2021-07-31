02399e0ac9f1364ae71a7a2c6477e2cd2fd7b80b
R8V <islaterm@gmail.com>
2021-07-30 21:15:47 -0400

    test(draw): The player can draw a card successfully | v0.1.1

--- src/test/kotlin/cl/ravenhill/makarena/model/PlayerTest.kt
+++ src/test/kotlin/cl/ravenhill/makarena/model/PlayerTest.kt
@@ -2,8 +2,10 @@ package cl.ravenhill.makarena.model
 
 import cl.ravenhill.makarena.model.player.Player
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.collections.shouldContain
 import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.collections.shouldNotBeIn
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.bind
 import io.kotest.property.arbitrary.list
@@ -18,33 +20,64 @@ class PlayerTest : StringSpec({
     forAll(nameGenerator()) { name -> Player(name) == Player(name) }
   }
   "Deck is created correctly" {
-    checkAll(Arb.list(cardGen, 45..60), nameGenerator()) {generated, playerName ->
+    checkAll(Arb.list(cardGen, 45..60), nameGenerator()) { generated, playerName ->
       val player = Player(playerName)
-      val cards = mutableListOf<Card>()
+      val cards = createCardPool(player, generated)
       player.deck shouldHaveSize 0
-      for (c in generated) {
-        cards.add(Card(c.name, c.attack))
-      }
-      for ((i, card) in cards.withIndex()) {
+      `move and check`(player, cards, player::addToDeck, player::deck)
+    }
+  }
+  "The player can draw a card successfully" {
+    checkAll(Arb.list(cardGen, 45..60), nameGenerator()) { generated, playerName ->
+      val player = Player(playerName)
+      val cards = createCardPool(player, generated)
+      for (card in cards) {
         player.addToDeck(card)
-        player.deck shouldHaveSize i + 1
-        player.deck shouldContain card
+      }
+      player.hand shouldHaveSize 0
+      var handSize = 0
+      var deckSize = player.deck.size
+      while (player.deck.isNotEmpty()) {
+        val movedCard = player.deck.first()
+        player.draw()
+        player.hand shouldHaveSize ++handSize
+        player.deck shouldHaveSize --deckSize
+        movedCard shouldNotBeIn player.deck
+        movedCard shouldBeIn player.hand
       }
     }
   }
-//  "The player can draw a card successfully" {
-//    val generators = Array(2) { cardGen }
-//    forAll(Arb.list(cardGen, 1..20), nameGenerator()) { cards, playerName ->
-//      val player = Player(playerName)
-//      player.hand.size shouldBe 0
-//      for (i in cards.indices) {
-//        player.draw()
-//        player.hand.size shouldBe i
-//      }
-//      true
-//    }
-//  }
 })
+
+/**
+ * Adds a card to a list and check the result.
+ *
+ * @param player Player
+ *    test player
+ * @param cards List<Card>
+ *    the list of cards to add
+ */
+fun `move and check`(
+  player: Player,
+  cards: List<Card>,
+  adder: (Card) -> Unit,
+  getter: () -> List<Card>
+) {
+  for ((i, card) in cards.withIndex()) {
+    adder(card)
+    player.deck shouldHaveSize i + 1
+    player.deck shouldContain card
+  }
+}
+
+fun createCardPool(player: Player, source: List<CardGen>): List<Card> {
+  val pool = mutableListOf<Card>()
+  for (c in source) {
+    pool.add(Card(c.name, c.attack))
+  }
+  return pool
+}
+
 
 data class CardGen(val name: String, val attack: Int)
 
