92a37d740c243f1b73d0c65db2ab903b9f4b7ad9
R8V <ignacio.slater@ug.uchile.cl>
2021-10-14 15:04:12 -0300

    doc(cards) | Base card class documentation 0.0.1 | WORKING

--- src/main/kotlin/cl/ravenhill/makarena/model/Card.kt
+++ src/main/kotlin/cl/ravenhill/makarena/model/Card.kt
@@ -1,3 +1,12 @@
 package cl.ravenhill.makarena.model
 
-data class Card(val name: String, val attack: Int)
+/**
+ * A card is the main entity of the game.
+ *
+ * @property name
+ *    The name of the card. This also serves as a unique identifier.
+ * @constructor
+ *    Creates a new card with the given name; note that this name should be unique, as two cards
+ *    with the same name will be considered the same.
+ */
+data class Card(val name: String)
