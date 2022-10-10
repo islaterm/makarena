dd14b434a5a11353481c712745e7a8717c719aef
R8V <ignacio.slater@ug.uchile.cl>
2022-10-10 10:52:37 -0300

    TEST Define empty test cases
    v0.2.2210.3

--- src/test/kotlin/cl/ravenhill/makarena/model/TicTacToeBoardSpec.kt
+++ src/test/kotlin/cl/ravenhill/makarena/model/TicTacToeBoardSpec.kt
@@ -4,17 +4,21 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class TicTacToeBoardSpec : StringSpec({
-    lateinit var board: TicTacToeBoard
-
-    beforeEach {
-        board = TicTacToeBoard.builder()
+    "An empty board should have no winner" {
+        val board = TicTacToeBoard.builder()
             .row('_', '_', '_')
             .row('_', '_', '_')
             .row('_', '_', '_')
             .build()
+        board.winner shouldBe '_'
     }
 
-    "An empty board should have no winner" {
-        board.winner shouldBe '_'
+    "A board with a single row of the same mark should have that mark as winner" {
+    }
+
+    "A board with a single column of the same mark should have that mark as winner" {
+    }
+
+    "A board with a single diagonal of the same mark should have that mark as winner" {
     }
 })
