5fac6e7d6e76ef5a1bd2159be3e62209a1249cf5
R8V <ignacio.slater@ug.uchile.cl>
2022-10-14 16:58:38 -0300

    FEAT Second cell button
    v0.2.2210.20

--- src/main/kotlin/cl/ravenhill/makarena/gui/GameView.kt
+++ src/main/kotlin/cl/ravenhill/makarena/gui/GameView.kt
@@ -6,22 +6,25 @@
  *  work. If not, see <https://creativecommons.org/licenses/by/4.0/>.
  */
 
+@file:Suppress("FunctionName")
+
 package cl.ravenhill.makarena.gui
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import cl.ravenhill.makarena.driver.exceptions.InvalidMoveException
 import cl.ravenhill.makarena.driver.ttt.TicTacToeGame
 import cl.ravenhill.makarena.driver.ttt.TicTacToeMark
 import cl.ravenhill.makarena.driver.ttt.TicTacToeMark.Empty
@@ -41,19 +44,29 @@ fun main() = application {
         }
         MaterialTheme {
             Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-                Button(
-                    modifier = Modifier
-                        .align(Alignment.Start)
-                        .padding(5.dp)
-                        .height(50.dp)
-                        .width(50.dp),
-                    onClick = {
-                        cells[0] = TicTacToeGame.player
-                        TicTacToeGame.move(0, 0)
-                    }) {
-                    Text("${cells[0]}", fontSize = 30.sp)
-                }
+                CellButton(cells, 0, 0)
+                CellButton(cells, 0, 1)
             }
         }
+    }
+}
+
+@Composable
+fun CellButton(cells: SnapshotStateList<TicTacToeMark>, i: Int, j: Int) {
+    Button(
+        modifier = Modifier
+            .padding(5.dp)
+            .height(50.dp)
+            .width(50.dp),
+        onClick = {
+            val player = TicTacToeGame.player
+            try {
+                TicTacToeGame.move(i, j)
+                cells[i * 3 + j] = player
+            } catch (e: InvalidMoveException) {
+                println(e.localizedMessage) // TODO: Change for logger
+            }
+        }) {
+        Text("${cells[i * 3 + j]}", fontSize = 30.sp)
     }
 }
